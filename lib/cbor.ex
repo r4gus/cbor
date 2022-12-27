807ec7036f52fd5c8ad2ca4d3bb3987d51229aed
David P. Sugar <david@thesugar.de>
2022-12-27 16:49:16 +0100

    decoder supports arrays

--- lib/cbor.ex
+++ lib/cbor.ex
@@ -11,11 +11,23 @@ defmodule Cbor do
       {1, value, rest} -> {:ok, -1 - value, rest}
       {2, value, rest} -> decode(:str, rest, value)
       {3, value, rest} -> decode(:str, rest, value)
+      {4, value, rest} -> decode(:arr, rest, value)
     end
   end
 
   defp decode(:str, <<data::binary>>, length) when byte_size(data) >= length, do: {:ok, binary_part(data, 0, length), binary_part(data, length, 0)}
   defp decode(:str, <<_::binary>>, _length), do: {:error, :insufficient_data}
+
+  defp decode(:arr, <<data::binary>>, 0), do: {:ok, [], data}
+  defp decode(:arr, <<data::binary>>, count) do
+    case decode(data) do
+      {:ok, data_item, rest} -> case decode(:arr, rest, count - 1) do
+        {:ok, array, rest} -> {:ok, [data_item] ++ array, rest}
+        {:error, error} -> {:error, error}
+      end
+      {:error, error} -> {:error, error}
+    end
+  end
 
   defp head(<<mt::3, 24::5, value::8, data::binary>>), do: {mt, value, data}
   defp head(<<mt::3, 25::5, value::16, data::binary>>), do: {mt, value, data}
