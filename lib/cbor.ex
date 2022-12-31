69c6699ebe7d9e016d75a7b6011fb48f1f98980d
David P. Sugar <david@thesugar.de>
2022-12-31 02:56:08 +0100

    map support added to decoeder

--- lib/cbor.ex
+++ lib/cbor.ex
@@ -12,18 +12,36 @@ defmodule Cbor do
       {2, value, rest} -> decode(:str, rest, value)
       {3, value, rest} -> decode(:str, rest, value)
       {4, value, rest} -> decode(:arr, rest, value)
+      {5, value, rest} -> decode(:map, rest, value)
     end
   end
 
-  defp decode(:str, <<data::binary>>, length) when byte_size(data) >= length, do: {:ok, binary_part(data, 0, length), binary_part(data, length, 0)}
+  defp decode(:str, <<data::binary>>, length) when byte_size(data) >= length do
+    {lhs, rhs} = String.split_at(data, length)
+    {:ok, lhs, rhs}
+  end
   defp decode(:str, <<_::binary>>, _length), do: {:error, :insufficient_data}
 
   defp decode(:arr, <<data::binary>>, 0), do: {:ok, [], data}
   defp decode(:arr, <<data::binary>>, count) do
     case decode(data) do
       {:ok, data_item, rest} -> case decode(:arr, rest, count - 1) do
-        {:ok, array, rest} -> {:ok, [data_item] ++ array, rest}
+        {:ok, array, rest} -> {:ok, [data_item | array], rest}
         {:error, error} -> {:error, error}
+      end
+      {:error, error} -> {:error, error}
+    end
+  end
+
+  defp decode(:map, <<data::binary>>, 0), do: {:ok, %{}, data}
+  defp decode(:map, <<data::binary>>, count) do
+    case decode(data) do
+      {:ok, key, key_rest} -> case decode(key_rest) do
+         {:ok, value, value_rest} -> case decode(:map, value_rest, count - 1) do
+            {:ok, map, rest} -> {:ok, Map.merge(map, %{key => value}), rest}
+            {:error, error} -> {:error, error}
+         end
+         {:error, error} -> {:error, error}
       end
       {:error, error} -> {:error, error}
     end
@@ -34,5 +52,6 @@ defmodule Cbor do
   defp head(<<mt::3, 26::5, value::32, data::binary>>), do: {mt, value, data}
   defp head(<<mt::3, 27::5, value::64, data::binary>>), do: {mt, value, data}
   defp head(<<mt::3, info::5, data::binary>>) when valid_info?(info), do: {mt, info, data}
-  defp head(<<_::binary>>), do: {:error, :not_well_formed}
+  defp head(<<mt::8, _::binary>>), do: {:error, "malformed head #{mt}"}
+  defp head(<<>>), do: {:error, "unexpected end of input"}
 end
